8e5a305978e00a647ea95744bc46d0146127c7ef
JasonVolkoff <kashijason@gmail.com>
2021-04-18 06:59:48 -0700

    Added redirect to second html for submitted info & redirect to home. CSS styling on second HTML.

--- dojo_server/userInfoApp/views.py
+++ dojo_server/userInfoApp/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 
 
 def index(request):
     return render(request, "index.html")
 
 
+def home(request):
+    return HttpResponseRedirect("/")
+
+
 def result(request):
-    print(request.method)
+    if request.method == "POST":
+        context = {
+            "name": request.POST["userName"],
+            "language": request.POST["userLanguage"],
+            "location": request.POST["userLocation"],
+            "comment": request.POST["userComment"],
+        }
+        return render(request, "userIndex.html", context)
     return render(request, "userIndex.html")
